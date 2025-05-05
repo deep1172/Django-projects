f3c7b3e58d2a0f1c5268f53e167183e1fb1b3b8b
Deepak Kumar <1172dipu@gmail.com>
2025-05-06 00:17:49 +0530

    feat: foodapp static files added

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -7,4 +7,5 @@ class Item(models.Model):
     
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
+    item_image = models.CharField(max_length=500 , default="https://www.shutterstock.com/image-vector/exciting-new-cafe-bar-restaurant-600nw-2145440019.jpg")
     item_price = models.IntegerField()
